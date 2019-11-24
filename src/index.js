553827a366119a707ad651b0b0b34d1142667d06
Carlos Viteri <carlos.viteri@verndale.com>
2019-11-24 00:48:56 -0500

    Add error handlers and standardize scaffolding

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
 import throttle from "lodash.throttle";
 
-const defaultOptions = {
+const defaultSettings = {
   className: "current",
   topOffset: 0
 };
 
 class ScrollSpy {
-  constructor(options) {
-    this.setOptions(options);
+  /**
+   *
+   * @param {Object} settings
+   * @param {NodeList} settings.navItems - nav items that are listening to the scroll
+   * @param {string} settings.className - className that will be applied to the "current" nav item
+   * @param {number} settings.topOffset - offset in pixels to compensate elements such as header in the calculation
+   *
+   */
+  constructor(settings) {
+    if (settings.navItems === "undefined") {
+      throw new Error(
+        "You must provide an element as an HTMLElement or NodeList"
+      );
+    }
+
+    this.setSettings(settings);
     this.setSections();
     this.addListeners();
     this.spySections();
   }
 
-  setOptions(options) {
-    this.options = {
-      ...defaultOptions,
-      ...options
+  setSettings(settings) {
+    this.settings = {
+      ...defaultSettings,
+      ...settings
     };
   }
 
   setSections() {
-    this.sections = Array.from(this.options.navItems).map(item =>
-      document.querySelector(item.getAttribute("href"))
-    );
+    this.sections = Array.from(this.settings.navItems).map(item => {
+      const href = item.getAttribute("href");
+
+      if (href === null) {
+        throw new Error(
+          "Make sure that all the nav items have a href attribute"
+        );
+      }
+
+      const section = document.querySelector(href);
+
+      if (section === null) {
+        throw new Error(
+          "Make sure that all the nav items have an asociated section with the href attribute"
+        );
+      }
+
+      return section;
+    });
   }
 
   addListeners() {
@@ -45,7 +75,7 @@ class ScrollSpy {
     const scrollTop = window.pageYOffset;
     const scrollBottom = scrollTop + window.innerHeight;
     const sectionTop =
-      section.getBoundingClientRect().top + scrollTop + this.options.topOffset;
+      section.getBoundingClientRect().top + scrollTop + this.settings.topOffset;
     const sectionBottom = sectionTop + section.offsetHeight;
 
     return sectionTop < scrollBottom && sectionBottom > scrollTop;
@@ -54,12 +84,12 @@ class ScrollSpy {
   markSections(visibilityStatus) {
     let isAlreadyMarked = false;
 
-    this.options.navItems.forEach((item, index) => {
+    this.settings.navItems.forEach((item, index) => {
       if (visibilityStatus[index] && !isAlreadyMarked) {
         isAlreadyMarked = true;
-        item.classList.add(this.options.className);
+        item.classList.add(this.settings.className);
       } else {
-        item.classList.remove(this.options.className);
+        item.classList.remove(this.settings.className);
       }
     });
   }
